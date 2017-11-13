87be51a20ae8f292f6ead68711fec9cbf6341f69
GenerateCode <JeremyBonar@Gmail.com>
2017-11-12 23:19:45 -0500

    more logic lesson 9

--- fundamentals/memory_game/js/main.js
+++ fundamentals/memory_game/js/main.js
@@ -2,23 +2,27 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-console.log("User flipped " + cardOne);
-cardsInPlay.push(cardOne); 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
+
 
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!"); 
-	} 
-	else {
-	alert("Sorry, try again"); 
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!"); 
+		} 
+		else {
+			alert("Sorry, try again"); 
+		}
 	}
-
 }
 
-if (cardsInPlay.length === 2) {
+
+
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
 	checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
+
