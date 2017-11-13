14e9fed71822103f4bb05b8d55445d8328921821
GenerateCode <JeremyBonar@Gmail.com>
2017-11-12 22:44:50 -0500

    created game logic

--- fundamentals/memory_game/js/main.js
+++ fundamentals/memory_game/js/main.js
@@ -1,6 +1,24 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
 console.log("User flipped " + cardOne);
+cardsInPlay.push(cardOne); 
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay);
+
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!"); 
+	} 
+	else {
+	alert("Sorry, try again"); 
+	}
+
+}
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
